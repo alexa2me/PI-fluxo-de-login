6887b80bdad9a248930a275f5deb75c14655633e
fernandagiacomel <79334817+fernandagiacomel@users.noreply.github.com>
2022-09-11 21:34:41 -0300

    Subindo correção.

--- logic/script.js
+++ logic/script.js
@@ -1,26 +1,20 @@
 //validação usuário
 function onChangeEmail() {
-  habilitaBotoes();
+  const emailValido = emailEhValido();
   errosEmail();
 }
 //validação senha
 function onChangeSenha() {
-  habilitaBotoes();
+  const senhaValida = senhaEhValida();
   errosSenha();
 }
 
 //puxa elementos dos HTML
 const form = {
-  //elementos e-mail:
+  //elemento e-mail:
   email: () => document.getElementById("email"),
-  emailInvalido: () => document.getElementById("emailInvalido"),
-  emailObrigatorio: () => document.getElementById("emailObrigatorio"),
-  //elementos senha
+  //elemento senha
   password: () => document.getElementById("password"),
-  passwordObrigatorio: () => document.getElementById("senhaObrigatoria"),
-  //elementos botões
-  recuperarSenha: () => document.getElementById("recuperarSenha"),
-  entrar: () => document.getElementById("btnEntrar"),
 };
 
 function emailEhValido() {
@@ -42,40 +36,6 @@ function senhaEhValida() {
 //verifica atraves de REGEX formatação do email (ex teste@teste.com)
 function validaEmail(email) {
   return /\S+@\S+\.\S+/.test(email);
-}
-
-//se campo email estiver vazio notifica erro, se email não seguir a formatação correta notifica erro
-function errosEmail() {
-  const email = form.email().value;
-  if (!email) {
-    form.emailObrigatorio().style.display = "block";
-  } else {
-    form.emailObrigatorio().style.display = "none";
-  }
-  if (validaEmail(email)) {
-    form.emailInvalido().style.display = "none";
-  } else {
-    form.emailInvalido().style.display = "block";
-  }
-}
-
-//se campo senha estiver vazio notifica erro
-function errosSenha() {
-  const password = form.password().value;
-  if (!password) {
-    form.passwordObrigatorio().style.display = "block";
-  } else {
-    form.passwordObrigatorio().style.display = "none";
-  }
-}
-
-//só habilita botões após verificar validade de email e senha
-function habilitaBotoes() {
-  const emailValido = emailEhValido();
-  form.recuperarSenha().disabled = !emailValido;
-
-  const senhaValida = senhaEhValida();
-  form.entrar().disabled = !emailValido || !senhaValida;
 }
 
 //função onclick que leva para home logada após clicar em entrar
